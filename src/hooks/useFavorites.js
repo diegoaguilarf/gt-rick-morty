e8a0f1ea1182142d1e359ffbf239193f7c0ed9de
Diego Aguilar <daafaguilar98@gmail.com>
2020-08-06 03:37:33 -0500

    create 2 hooks useDebounce: by search engine useFavorites: by save characters

---
+++ src/hooks/useFavorites.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+
+const useFavorites = data => {
+
+    const [isFavorite, setIsFavorite] = useState(false);
+    const [favorites, setFavorites] = useState(null);
+
+    useEffect(() => {
+        getFavorites()
+        if (data) {
+            validateFavorite()
+        }
+    }, [])
+
+    useEffect(() => {
+      if (data) {
+        validateFavorite()
+      }
+    }, [data])
+
+  const getFavorites = () => {
+    setFavorites(JSON.parse(localStorage.getItem("favoriteCharacters")))
+  };
+
+  const validateFavorite = () => {
+    let favoriteCharacters = localStorage.getItem("favoriteCharacters");
+    favoriteCharacters = JSON.parse(favoriteCharacters);
+    const existFavorite = favoriteCharacters.find(item => item.id == data.id);
+    setIsFavorite(existFavorite ? true : false);
+  };
+
+  const addFavorite = () => {
+    let favoriteCharacters = localStorage.getItem("favoriteCharacters");
+    if (favoriteCharacters) {
+      favoriteCharacters = JSON.parse(favoriteCharacters);
+      localStorage.setItem(
+        "favoriteCharacters",
+        JSON.stringify([data, ...favoriteCharacters])
+      );
+    } else {
+      localStorage.setItem("favoriteCharacters", JSON.stringify([data]));
+    }
+    setIsFavorite(true);
+  };
+
+  const removeFavorite = () => {
+    let favoriteCharacters = localStorage.getItem("favoriteCharacters");
+    favoriteCharacters = JSON.parse(favoriteCharacters);
+    const indexFavoriteCharacter = favoriteCharacters.findIndex(item => item.id === data.id);
+    favoriteCharacters.splice(indexFavoriteCharacter, 1);
+
+    localStorage.setItem("favoriteCharacters", JSON.stringify(favoriteCharacters));
+    setIsFavorite(false);
+  };
+
+  return { favorites, isFavorite, addFavorite, removeFavorite };
+};
+
+export default useFavorites;
